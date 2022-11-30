04e99f07ad47e4aa47429de792d9084beb8ca904
Fausto Ramirez <ramirezmedia@gmail.com>
2022-11-29 21:56:44 -0500

    commit tree 603858775d83b2d4da179fe4f61f58f0d750ba79

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(newCat){
+    cats.push(newCat);
+}
+
+function destructivelyPrependCat(cat){
+    cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(cat){
+    return [...cats, cat];
+}
+
+function prependCat(cat){
+    return [cat, ...cats];
+}
+
+function removeLastCat(cat){
+    return cats.slice(0, 2);
+}
+
+function removeFirstCat(cat){
+    return cats.slice(1);
+}
